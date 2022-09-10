868b4e032f7595db5b13bfd8ba69cd96da2bf0fa
Andp-25 <anudeeptalari25@gmail.com>
2022-09-10 23:20:20 +0530

    partition algo added

--- quick_sort.c
+++ quick_sort.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
 
+// function to find the partition position
+int partition(int array[], int low, int high) {
+
+  // select the rightmost element as pivot
+  int pivot = array[high];
+
+  // pointer for greater element
+  int i = (low - 1);
+
+  // traverse each element of the array
+  // compare them with the pivot
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+
+      // if element smaller than pivot is found
+      // swap it with the greater element pointed by i
+      i++;
+
+      // swap element at i with element at j
+      swap(&array[i], &array[j]);
+    }
+  }
+
+  // swap the pivot element with the greater element at i
+  swap(&array[i + 1], &array[high]);
+
+  // return the partition point
+  return (i + 1);
+}
 void quickSort(int array[], int low, int high) {
   if (low < high) {
 
